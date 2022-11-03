a42f88965463bb6b18646cbd32bee01e54afd6ce
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2022-11-03 07:00:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/get", async (req, res) => {
   console.log(url);
   const info = await ytdl.getInfo(url);
   const title = info.VideoDetails.title;
-  const thumbnail = info.VideoDetails.thumbnails[0].url;
+  const thumbnail = info.VideoDetails.thumbnails[19].url;
   let formats = info.formats;
 
   const audioFormats = ytdl.filterFormats(info.formats, "mp3");
